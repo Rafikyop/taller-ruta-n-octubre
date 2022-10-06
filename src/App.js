7fbf2b29779c729855621749ffe45edcee38fa36
Santiago Larrea <slarrea@proteccion.com.co>
2022-10-06 16:50:02 -0500

    Crear una pagina web que me muestra personajes de Rick and Morty.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
+import { useState, useEffect } from "react";
 import './App.css';
 
 function App() {
+  const [characters, setCharacters] = useState([])
+
+  async function getAllCharacters() {
+    const response = await fetch('https://rickandmortyapi.com/api/character')
+    const characters = await response.json()
+    setCharacters(characters.results)
+  }
+
+  function killAllCharacters() {
+    console.log('Matar a todos los personajes')
+    const killedCharacters = [ ...characters ]
+    killedCharacters.forEach(character => character.status = 'Dead')
+    setCharacters(killedCharacters)
+  }
+
+  useEffect(() => {
+    getAllCharacters()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="main">
+      <h1 className="title">Rick and Morty</h1>
+      <p className="description">Esta es una lista de los personajes de Rick and Morty</p>
+      <button
+        className="button"
+        onClick={killAllCharacters}
+      >
+        Matar todos los personajes
+      </button>
+      <section className="grid">
+        {characters?.map(character => (
+          <div className="card">
+            <img
+              className="image"
+              src={character.image}
+              alt={character.name}
+              width="380px"
+              height="380px"
+            />
+            <h2 className="name">{character.name}</h2>
+            <div className="info">
+              <span className={`status ${
+                character.status !== 'Alive' ? 'red-status' : ''
+              }`} />
+              <span className="text">{character.status} - {character.species}</span>
+            </div>
+          </div>
+        ))}
+      </section>
+    </section>
   );
 }
 
